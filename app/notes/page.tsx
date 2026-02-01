21f2c4611697a24ff41f227cec03dd2a821bb785
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2026-02-01 15:57:35 +0200

    fixed

---
+++ app/notes/page.tsx
@@ -0,0 +1,12 @@
+import css from './page.module.css'
+
+export default function NotesPage() {
+  return (
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>All Notes</h1>
+        {}
+      </div>
+    </main>
+  )
+}
